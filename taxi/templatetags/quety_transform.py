80495c866d24c7b2bb82a22c6eeb49a844067360
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-07-17 20:56:11 +0300

    Solution

--- taxi/templatetags/quety_transform.py
+++ taxi/templatetags/quety_transform.py
@@ -6,9 +6,9 @@ register = template.Library()
 @register.simple_tag
 def query_transform(request, **kwargs):
     updated = request.GET.copy()
-    for key, val in kwargs.items():
-        if val is not None:
-            updated[key] = val
+    for key, value in kwargs.items():
+        if value is not None:
+            updated[key] = value
         else:
             updated.pop(key, 0)
     return updated.urlencode()
